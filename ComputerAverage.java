75eb41cc3e656daec382d5a5be671b61ddfbe7df
Xie026 <71316959+Xie026@users.noreply.github.com>
2020-09-29 15:58:47 +0800

    Create ComputerAverage.java

---
+++ ComputerAverage.java
@@ -0,0 +1,20 @@
+package hello;
+
+import java.util.Scanner;
+
+public class hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		System.out.print("Enter three numbers:");
+		double number1=input.nextDouble();
+		double number2=input.nextDouble();
+		double number3=input.nextDouble();
+		double average=(number1+number2+number3)/3;
+		System.out.println("The average of"+number1+" "+number2+" "+number3+"is"+average);
+	}
+
+}
+
+从键盘读取多个输入的例子，读取三个数值，然后显示他们的平均值。
